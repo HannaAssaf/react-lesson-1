8098fde24bdcac74d209147995da9ffa88924f97
Hanna Assaf <145717934+HannaAssaf@users.noreply.github.com>
2025-04-14 22:01:44 +0100

    3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,12 @@
+import PaymentCard from "./PaynentCard";
+
 export default function App() {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <>
+      <h1>Payments</h1>
+      <PaymentCard amount={100} />
+      <PaymentCard amount={400} />
+      <PaymentCard amount={3900} />
+    </>
   );
 }
---
+++ src/components/PaynentCard.jsx
@@ -0,0 +1,9 @@
+export default function PaymentCard({ amount }) {
+  return (
+    <div>
+      <p>Amount: {amount}</p>
+      {/* <p>Expiry Date: {props.expiryDate}</p>
+            <p>Card Holder Name: {props.cardHolderName}</p> */}
+    </div>
+  );
+}
